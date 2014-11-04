d735640be5fab1e92e8914d1f67ba062aae1bed0
David\ Beitey <david@davidjb.com>
2014-11-04 15:20:00 +1000

    Change theme, update resume

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 ./bin/py yaml2json.py cv.yaml output/resume.json
 cd output/
-../bin/resume export cv --format pdf
-../bin/resume export index --format html
+../bin/resume export cv --format pdf --theme elegant-compact
+../bin/resume export index --format html --theme elegant-compact
 cd ..
 ./bin/ghp-import -n -p -m "Update CV" output/
